1b78cc81aa4040055780d3e9015896294fdf2048
Avery Bross <farmer@allheartsfarm.com>
2025-09-17 21:54:32 -0400

    VROOM: force IP address 66.33.22.61 for Valhalla host

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -13,8 +13,8 @@ RAW_VALHALLA_HOST=${VALHALLA_HOST:-allheartsfarm-valhalla.up.railway.app}
 VALHALLA_HOST_CLEAN=${RAW_VALHALLA_HOST#http://}
 VALHALLA_HOST_CLEAN=${VALHALLA_HOST_CLEAN#https://}
 VALHALLA_HOST_CLEAN=${VALHALLA_HOST_CLEAN%/}
-# Use IP address to avoid DNS issues
-VALHALLA_HOST_CLEAN=${VALHALLA_HOST_CLEAN:-66.33.22.61}
+# Force IP address to avoid DNS issues
+VALHALLA_HOST_CLEAN=66.33.22.61
 VALHALLA_USE_HTTPS_NORM=$(echo "${VALHALLA_USE_HTTPS:-true}" | tr '[:upper:]' '[:lower:]')
 if [ -n "${VALHALLA_PORT:-}" ]; then
   VALHALLA_PORT_EFF=${VALHALLA_PORT}
